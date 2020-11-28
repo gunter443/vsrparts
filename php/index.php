fc06398b2f89e14e4b08c1d1aa24c58079762663
gunter443 <joseluisjllm97@gmail.com>
2020-11-28 20:27:49 +0100

    ultimo

---
+++ php/index.php
@@ -0,0 +1,27 @@
+<?php
+include_once "conexionBBDD.php";
+//session_start();
+//if (!$_SESSION['sesion']) {
+ //   header("location:inicio.php");
+//}
+//$sql = $conexion->query("SELECT * FROM emple;"); /*ejecuta el select para mostrar los datos desde el inicio de la página*/
+//$personas = $sql->fetchAll(PDO::FETCH_OBJ);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="../fotos/icono.png" type="image/x-icon">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Página Principal</title>
+</head>
+<body>
+    
+</body>
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+<script src="./"></script>
+</html>
